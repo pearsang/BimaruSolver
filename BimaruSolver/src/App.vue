b4190b1a528377fa965ba7ea8afdd44cc4ff9b13
José Pereira <jose.a.pereira03@gmail.com>
2023-05-25 22:57:51 +0100

    Initial code

---
+++ BimaruSolver/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Board /> 
+</template>
+
+<script setup lang="ts">
+import Board from './components/Board.vue';
+</script>
+
+<style>
+#app {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+}
+
+body {
+  background-color: rgb(228, 249, 245)
+}
+
+
+
+</style>
