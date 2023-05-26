60e8fb593e327ed81a8e8151012a293ee52d6a23
simaosanguinho <simao.sanguinho@gmail.com>
2023-05-26 15:26:36 +0100

    messed up

--- BimaruSolver/src/components/TopBar.vue
+++ BimaruSolver/src/components/TopBar.vue
@@ -1,26 +1,33 @@
 <template>
-    <v-card
-      class="mx-auto mb-16"
-      max-width="448"
-    >
-        <v-app-bar
-          color="grey-darken-4"
-          class="mx-auto"
-          elevation="0"
-        >
-        <v-app-bar-title 
-            class="text-left font-weight-medium "
-            style="font-size: 30px, font-family: 'Roboto', sans-serif;"
-            color="rgb(64, 81, 78)"
-        >
-            BimaruSolver
-        </v-app-bar-title>
-        </v-app-bar>
-  
-    
-    </v-card>
+  <v-card class="mx-auto mb-16" max-width="448">
+    <v-app-bar color="rgb(48, 227, 202)" class="mx-auto" elevation="3">
+      <v-app-bar-title
+        class="text-left font-weight-medium"
+        style="font-size: 20px"
+        color="rgb(64, 81, 78)"
+      >
+        BimaruSolver
+      </v-app-bar-title>
+
+      <v-app-bar-nav-icon class="mr-14 mt-0">
+        <v-img
+          height="100"
+          width="100"
+          :src="imageSrc"
+        ></v-img>
+      </v-app-bar-nav-icon>
+      
+    </v-app-bar>
+  </v-card>
 </template>
 
+<style lang="css"></style>
 
-<style lang="css">
-</style> 
+<script setup>
+  import { computed } from "vue";
+  const imageSrc = computed(() => {
+        return new URL("../assets/boat.png", import.meta.url).href;
+      });
+</script>   
+
+
