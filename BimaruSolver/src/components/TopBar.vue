68d2f1863e67465b61cd946b43e4181533dc11f8
simaosanguinho <simao.sanguinho@gmail.com>
2023-05-26 16:47:38 +0100

    add top bar

--- BimaruSolver/src/components/TopBar.vue
+++ BimaruSolver/src/components/TopBar.vue
@@ -1,22 +1,19 @@
 <template>
   <v-card class="mx-auto mb-16" max-width="448">
-    <v-app-bar color="rgb(48, 227, 202)" class="mx-auto" elevation="3">
+    <v-app-bar color="grey-darken-4" elevation="3">
       <v-app-bar-title
-        class="text-left font-weight-medium"
-        style="font-size: 20px"
-        color="rgb(64, 81, 78)"
+        class="d-flex justify-star"
+        style="
+          font-family: 'Roboto Mono', monospace;
+          font-size: 30px;
+        "
       >
         BimaruSolver
       </v-app-bar-title>
 
-      <v-app-bar-nav-icon class="mr-14 mt-0">
-        <v-img
-          height="100"
-          width="100"
-          :src="imageSrc"
-        ></v-img>
-      </v-app-bar-nav-icon>
-      
+      <v-sheet class="d-flex justify-end" color="grey-darken-4">
+        <v-img height="90" width="150" :src="imageSrc"></v-img>
+      </v-sheet>
     </v-app-bar>
   </v-card>
 </template>
@@ -24,10 +21,8 @@
 <style lang="css"></style>
 
 <script setup>
-  import { computed } from "vue";
-  const imageSrc = computed(() => {
-        return new URL("../assets/boat.png", import.meta.url).href;
-      });
-</script>   
-
-
+import { computed } from "vue";
+const imageSrc = computed(() => {
+  return new URL("../assets/logo.png", import.meta.url).href;
+});
+</script>
