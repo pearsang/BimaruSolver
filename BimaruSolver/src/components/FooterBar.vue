d4c1b97aa30c7b15ee8f24dab38cb9ecc44c2330
simaosanguinho <simao.sanguinho@gmail.com>
2023-05-26 18:29:20 +0100

    footer bug fixed

--- BimaruSolver/src/components/FooterBar.vue
+++ BimaruSolver/src/components/FooterBar.vue
@@ -5,6 +5,7 @@
       height="50px"
       class="d-flex align-center justify-center mb-0 pb-0"
       color="grey-darken-4"
+      elevation="0"
     >
       <a href="" class="mx-2"> <v-icon icon="mdi-github"></v-icon> Pearsang</a>
       | Made by José Pereira and Simão Sanguinho | &copy; 2023
